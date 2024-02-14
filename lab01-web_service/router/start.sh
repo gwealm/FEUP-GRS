182b3c67c293759d54c9950b5651a6a25345e570
Guilherme Almeida <gui1612@sapo.pt>
2024-02-14 15:00:52 +0000

    feat: configures nat

--- lab01-web_service/router/start.sh
+++ lab01-web_service/router/start.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 /sbin/ip route replace default via 10.0.3.1
+
+## NAT ##
+
+# Is this really relevant??
+# iptables -t nat --delete-chain
+
+iptables -t nat -A POSTROUTING --out-interface eth1 -j MASQUERADE
+iptables -A FORWARD --in-interface eth2 -j ACCEPT
+iptables -A FORWARD --in-interface eth3 -j ACCEPT
+
+# Should we restart the service ???
+
+echo 1 > /proc/sys/net/ipv4/ip_forward

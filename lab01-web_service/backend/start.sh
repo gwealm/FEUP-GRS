f91053cfeda0fa3bad1b1d67991c12b5cb420631
Guilherme Almeida <gui1612@sapo.pt>
2024-02-22 03:59:18 +0000

    feat: tries to add server name

--- lab01-web_service/backend/start.sh
+++ lab01-web_service/backend/start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-/sbin/ip route replace default via 10.0.2.6
-nginx -g "daemon off;"
-
 
 DOCKERINFO=$(curl -s --unix-socket /run/docker.sock http://docker/containers/$HOSTNAME/json)
 ID=$(python3 -c "import sys, json; print(json.loads(sys.argv[1])[\"Name\"].split(\"_\")[-1])" "$DOCKERINFO")
 
-# echo "This is node $ID"
 
-export SERVER_ID=$ID
+echo "export SERVER_ID=$ID" >> ~/.bashrc
+echo "export SERVER_ID=$ID" >> ~/id.txt
+source ~/.bashrc
+
+echo "SUCESS"
+
+/sbin/ip route replace default via 10.0.2.6
+nginx -g "daemon off;"
+
+

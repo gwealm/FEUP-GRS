141b2eb886ce868e5a701e292f8bf41fd5b43b11
Guilherme Almeida <gui1612@sapo.pt>
2024-02-22 03:00:09 +0000

    fix: fixes load balancing

---
+++ lab01-web_service/backend/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+/sbin/ip route replace default via 10.0.2.6
+nginx -g "daemon off;"
+
+
+# DOCKERINFO=$(curl -s --unix-socket /run/docker.sock http://docker/containers/$HOSTNAME/json)
+# ID=$(python3 -c "import sys, json; print(json.loads(sys.argv[1])[\"Name\"].split(\"_\")[-1])" "$DOCKERINFO")
+
+# echo "This is node $ID"
+
+# export SERVER_ID=$ID
--- lab01-web_service/server/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/sbin/ip route replace default via 10.0.2.6
-nginx -g "daemon off;"

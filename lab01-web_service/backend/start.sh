8bf48c7ea9a9def2ed6866690cc274252ce946b4
Guilherme Almeida <gui1612@sapo.pt>
2024-02-22 03:39:27 +0000

    feat: adds server id and ssi

--- lab01-web_service/backend/start.sh
+++ lab01-web_service/backend/start.sh
@@ -3,9 +3,9 @@
 nginx -g "daemon off;"
 
 
-# DOCKERINFO=$(curl -s --unix-socket /run/docker.sock http://docker/containers/$HOSTNAME/json)
-# ID=$(python3 -c "import sys, json; print(json.loads(sys.argv[1])[\"Name\"].split(\"_\")[-1])" "$DOCKERINFO")
+DOCKERINFO=$(curl -s --unix-socket /run/docker.sock http://docker/containers/$HOSTNAME/json)
+ID=$(python3 -c "import sys, json; print(json.loads(sys.argv[1])[\"Name\"].split(\"_\")[-1])" "$DOCKERINFO")
 
 # echo "This is node $ID"
 
-# export SERVER_ID=$ID
+export SERVER_ID=$ID

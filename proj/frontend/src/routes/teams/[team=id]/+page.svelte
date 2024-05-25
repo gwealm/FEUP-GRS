bb962ad4bfbd143378500fb114b10b1167817024
Nuno Pereira <nunoafonso2002@gmail.com>
2024-05-25 20:03:15 +0100

    Added better UX for team deletion#

--- proj/frontend/src/routes/teams/[team=id]/+page.svelte
+++ proj/frontend/src/routes/teams/[team=id]/+page.svelte
@@ -11,6 +11,7 @@
 	export let data: PageData;
 
 	$: team = data.team!;
+	let showLoading = false;
 </script>
 
 <div class="mx-2 my-10 px-2 py-10 lg:mx-28 lg:px-28">
@@ -49,9 +50,7 @@
 						<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
 						<AlertDialog.Action
 							on:click={async () => {
-								const toastId = toast.info(`Deleting ${team.name}`, {
-									description: new Date().toLocaleString()
-								});
+								showLoading = true;
 
 								// TODO: Implement team deletion service/API call, dismiss on response.
 								await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -59,7 +58,6 @@
 								toast.info(`Deleted ${team.name}`, {
 									description: new Date().toLocaleString()
 								});
-								toast.dismiss(toastId);
 
 								await goto('/');
 							}}>Continue</AlertDialog.Action
@@ -82,3 +80,57 @@
 		</Tabs.Content>
 	</Tabs.Root>
 </div>
+{#if showLoading}
+	<div class="absolute left-0 top-0 h-full w-full">
+		<div class="absolute left-0 top-0 flex h-full w-full backdrop-blur"></div>
+		<div
+			class="absolute left-1/2 top-1/2 flex h-1/2 w-[40em] max-w-[40em] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-5"
+		>
+			<h2 class="text-xl">Deleting team "{team.name}"</h2>
+			<span class="loader"></span>
+		</div>
+	</div>
+{/if}
+
+<style lang="postcss">
+	.loader {
+		width: 48px;
+		height: 48px;
+		border-radius: 50%;
+		position: relative;
+		animation: rotate 1s linear infinite;
+	}
+	.loader::before {
+		content: '';
+		box-sizing: border-box;
+		position: absolute;
+		inset: 0px;
+		border-radius: 50%;
+		border: 5px solid #fff;
+		animation: prixClipFix 2s linear infinite;
+	}
+
+	@keyframes rotate {
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+
+	@keyframes prixClipFix {
+		0% {
+			clip-path: polygon(50% 50%, 0 0, 0 0, 0 0, 0 0, 0 0);
+		}
+		25% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 0, 100% 0, 100% 0);
+		}
+		50% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 100% 100%, 100% 100%);
+		}
+		75% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%);
+		}
+		100% {
+			clip-path: polygon(50% 50%, 0 0, 100% 0, 100% 100%, 0 100%, 0 0);
+		}
+	}
+</style>

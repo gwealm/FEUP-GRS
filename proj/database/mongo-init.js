71e93b8950e34ff8968b43cc84ec3d8bb478acef
Guilherme Almeida <gui1612@sapo.pt>
2024-05-28 04:30:40 +0100

    feat: adds a variety of services

--- proj/database/mongo-init.js
+++ proj/database/mongo-init.js
@@ -42,4 +42,116 @@ db.services.insertMany([
         image: "ubuntu/squid:latest",
         tag: "Proxy"
     },
+    {
+        name: 'Nginx',
+        slug: 'nginx',
+        description: 'Nginx Web Server',
+        image: "nginx",
+        tag: "Web"
+    },
+    {
+        name: 'WordPress',
+        slug: 'wordpress',
+        description: 'WordPress Content Management System',
+        image: "wordpress",
+        tag: "Web"
+    },
+    {
+        name: 'Jenkins',
+        slug: 'jenkins',
+        description: 'Jenkins Continuous Integration and Delivery',
+        image: "jenkins/jenkins",
+        tag: "CI/CD"
+    },
+    {
+        name: 'Postgres',
+        slug: 'postgres',
+        description: 'PostgreSQL Database Service',
+        image: "postgres",
+        tag: "DB"
+    },
+    {
+        name: 'Elasticsearch',
+        slug: 'elasticsearch',
+        description: 'Elasticsearch Search Engine',
+        image: "elasticsearch",
+        tag: "Search"
+    },
+    {
+        name: 'RabbitMQ',
+        slug: 'rabbitmq',
+        description: 'RabbitMQ Message Broker',
+        image: "rabbitmq",
+        tag: "Messaging"
+    },
+    {
+        name: 'Kafka',
+        slug: 'kafka',
+        description: 'Apache Kafka Stream Processing',
+        image: "kafka",
+        tag: "Streaming"
+    },
+    {
+        name: 'Prometheus',
+        slug: 'prometheus',
+        description: 'Prometheus Monitoring System',
+        image: "prom/prometheus",
+        tag: "Monitoring"
+    },
+    {
+        name: 'Grafana',
+        slug: 'grafana',
+        description: 'Grafana Data Visualization',
+        image: "grafana/grafana",
+        tag: "Monitoring"
+    },
+    {
+        name: 'Zulip',
+        slug: 'zulip',
+        description: 'Zulip Team Chat and Collaboration',
+        image: "zulip/zulip",
+        tag: "Messaging"
+    },
+    {
+        name: 'HAProxy',
+        slug: 'haproxy',
+        description: 'HAProxy Load Balancer',
+        image: "haproxy",
+        tag: "Proxy"
+    },
+    {
+        name: 'Consul',
+        slug: 'consul',
+        description: 'Consul Service Discovery and Configuration',
+        image: "consul",
+        tag: "Service Discovery"
+    },
+    {
+        name: 'Vault',
+        slug: 'vault',
+        description: 'Vault Secrets Management',
+        image: "vault",
+        tag: "Security"
+    },
+    {
+        name: 'Traefik',
+        slug: 'traefik',
+        description: 'Traefik Reverse Proxy and Load Balancer',
+        image: "traefik",
+        tag: "Proxy"
+    },
+    {
+        name: 'Fluentd',
+        slug: 'fluentd',
+        description: 'Fluentd Log Collector',
+        image: "fluent/fluentd",
+        tag: "Logging"
+    },
+    {
+        name: 'Logstash',
+        slug: 'logstash',
+        description: 'Logstash Data Processing Pipeline',
+        image: "logstash",
+        tag: "Logging"
+    }
 ]);
